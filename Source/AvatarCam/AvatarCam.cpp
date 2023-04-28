848a7cc29f54ae6f6ff83fda2b33445758880c59
Tsung-Hua Tsai <tsai.tsunghua@gmail.com>
2023-04-28 14:41:59 +0800

    Initial version.

---
+++ Source/AvatarCam/AvatarCam.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "AvatarCam.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, AvatarCam, "AvatarCam" );
+ 
